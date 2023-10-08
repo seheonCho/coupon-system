74a78ad2058c90314be2a29120b5111eff4ec5ae
seheonCho <whtpgjs94@gmail.com>
2023-10-08 13:55:52 +0900

    3. 한번만 쿠폰 응모하는 테스트 작성
    - 요청 한 개에 대한 처리 검증만 이루어짐

---
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.api.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.api.repository.CouponRepository;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+	@Autowired
+	private ApplyService applyService;
+
+	@Autowired
+	private CouponRepository couponRepository;
+
+	@Test
+	@DisplayName("한번만 응모")
+	void applyOneTimes() {
+		applyService.apply(1L);
+
+		long count = couponRepository.count();
+
+		assertThat(count).isEqualTo(1);
+	}
+
+}
