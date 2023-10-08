10baecf0911e357eb8fb7e04855711e00983790b
seheonCho <whtpgjs94@gmail.com>
2023-10-08 14:23:49 +0900

    4. 여러명 동시 응모 테스트
    - MultiThread 테스트 환경 구현
      - 테스트 실패

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -3,6 +3,10 @@ package com.example.api.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,6 +32,33 @@ class ApplyServiceTest {
 		long count = couponRepository.count();
 
 		assertThat(count).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("여러명 응모")
+	void applyManyTimes() throws InterruptedException {
+	    int threadCount = 1000;
+
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			long userId = i;
+			executorService.submit(() -> {
+				try {
+					applyService.apply(userId);
+				} finally {
+					countDownLatch.countDown();
+				}
+			});
+		}
+
+		countDownLatch.await();
+
+		long count = couponRepository.count();
+
+		assertThat(count).isEqualTo(100);
+
 	}
 
 }
